fbdf9f3d59adab694189268a015f4d68091745b5
chartistx <chartistx@gmail.com>
2024-04-18 18:48:24 +0300

    added dependency ejs(view engine)

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+//adding view engine
+app.set('view engine', 'ejs');
 
-app.listen(3000, () => {
-    console.log('Server is running.');
-});
-
+//requests
 app.get('/', (req, res) => {
     res.send('test');
+});
+
+
+
+
+
+
+//listen port 3000
+app.listen(3000, () => {
+    console.log('Server is running.');
 });
